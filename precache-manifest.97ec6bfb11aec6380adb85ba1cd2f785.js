84a4b11a5495f2501f48f14e8b351b3490ad884e
Danil Stoyanov <d.stoyanov@corp.mail.ru>
2020-09-18 10:28:36 +0300

    Updates

---
+++ precache-manifest.97ec6bfb11aec6380adb85ba1cd2f785.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d81c26427ff34fae7f1ca0248baa49c",
+    "url": "/podcastweb/index.html"
+  },
+  {
+    "revision": "3dadf92128dc7a6efa0a",
+    "url": "/podcastweb/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "59ead3191a0298b73aee",
+    "url": "/podcastweb/static/css/main.a5cb8d74.chunk.css"
+  },
+  {
+    "revision": "3dadf92128dc7a6efa0a",
+    "url": "/podcastweb/static/js/2.e4f1023f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/podcastweb/static/js/2.e4f1023f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59ead3191a0298b73aee",
+    "url": "/podcastweb/static/js/main.87dd8ddd.chunk.js"
+  },
+  {
+    "revision": "7fa4e53c18703e6d09d5",
+    "url": "/podcastweb/static/js/runtime-main.ca431895.js"
+  }
+]);
